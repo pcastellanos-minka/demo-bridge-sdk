41ee7de362ef3b9fce9aab2feec2eff78214cb46
pcastellanos-minka <paula@minka.io>
2024-09-27 13:05:14 -0500

    added intent create logic

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,8 @@ import { cleanEnv, host, num, port, str } from 'envalid'
 export const config = cleanEnv(process.env, {
   BRIDGE_PUBLIC_KEY: str({ desc: 'Bridge public key' }),
   BRIDGE_SECRET_KEY: str({ desc: 'Bridge private key' }),
+  INTENT_PUBLIC_KEY: str({ desc: 'Signer to create intent - public key' }),
+  INTENT_PRIVATE_KEY: str({ desc: 'Signer to create intent  - private key' }),
   LEDGER_HANDLE: str({ desc: 'Ledger name' }),
   LEDGER_SERVER: str({ desc: 'Ledger URL' }),
   LEDGER_PUBLIC_KEY: str({ desc: 'Ledger public key' }),
---
+++ src/intent/create.intent.js
@@ -0,0 +1,96 @@
+
+const { LedgerSdk } = require('@minka/ledger-sdk')
+const { nanoid } = require('nanoid')
+
+const encode = 'ed25519-raw';
+
+const config = {
+    LEDGER_SERVER: "https://ldg-stg.one/api/v2",
+    LEDGER_HANDLE: "achco",
+    INTENT_PUBLIC_KEY: "SB/1WQzIujGxHVz9Yt54zpj+MedKT6uKfionadT0PzU=",
+    INTENT_PRIVATE_KEY:"zVq1QjJVDTobAw/OPBRZypzP9ecTuPRhP1qNylS73J0="
+}
+
+const sdk = new LedgerSdk({
+    server: config.LEDGER_SERVER,
+    ledger: config.LEDGER_HANDLE
+})
+
+
+
+async function run() {
+
+    console.log("start")
+    const { intent } = await sdk.intent
+        .init()
+        .data({
+                "handle": nanoid(17),
+                "claims": [
+                    {
+                        "action": "transfer",
+                        "amount": 1000,
+                        "source": {
+                            "custom": {
+                                "name": "Andrés Castaño",
+                                "type": "individual",
+                                "identificationType": "cc",
+                                "identification": "15743215698",
+                                "emailAddress":"andres@gmail.com",
+                                "mobileNumber":"+573126549870"
+                            },
+                            "handle": "caho:121212@teslabank.io"
+                        },
+                        "symbol": {
+                            "handle": "cop"
+                        },
+                        "target": {
+                            "custom": {
+                                "name": "Gloria Rubiano",
+                                "type": "individual",
+                                "identificationType": "cc",
+                                "identification": "157454987698",
+                                "emailAddress":"gloria@gmail.com",
+                                "mobileNumber":"+573123350257"
+                            },
+                            "handle": "caho:424242@mintbank.dev"
+                        }
+                    }
+                ],
+                "schema": "transfer",
+                "access": [
+                    {
+                        "action": "any",
+                        "signer": {
+                            "public": config.INTENT_PUBLIC_KEY
+                        }
+                    },
+                    {
+                        "action": "read",
+                        "bearer": {
+                            "$signer": {
+                                "public": config.INTENT_PUBLIC_KEY
+                            }
+                        }
+                    }
+                ],
+                "config": {
+                    "commit": "auto"
+                }
+            } 
+        )
+        .hash()
+        .sign([
+            {
+                keyPair: {
+                    public: config.INTENT_PUBLIC_KEY,
+                    format: encode,
+                    secret: config.INTENT_PRIVATE_KEY,
+                },
+            },
+        ])
+        .send()
+
+        console.log(`Intent created: ${JSON.stringify(intent, null, 2)}`);
+}
+
+run()
